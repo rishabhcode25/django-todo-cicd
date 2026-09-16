7f120859871ef6bd79f52035f13a7e554a74a9e9
Rishabh Verma <rishabhverma25106@gmail.com>
2026-09-16 12:03:06 +0530

    Fix Todo primary key warning

--- todos/models.py
+++ todos/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Todo(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=100)
+    created_at = models.DateTimeField('Created', auto_now_add=True)
+    update_at = models.DateTimeField('Updated', auto_now=True)
+    isCompleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
     created_at = models.DateTimeField('Created', auto_now_add=True)
     update_at = models.DateTimeField('Updated', auto_now=True)
     isCompleted = models.BooleanField(default=False)
